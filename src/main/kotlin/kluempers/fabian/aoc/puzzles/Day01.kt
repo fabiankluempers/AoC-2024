8698994af41c93a9cef2d3b5fe3a7a970f0dd4c5
Fabian Klümpers <kluempers.fabian@gmail.com>
2024-12-01 19:31:14 +0100

    Improve Day01

--- src/main/kotlin/kluempers/fabian/aoc/puzzles/Day01.kt
+++ src/main/kotlin/kluempers/fabian/aoc/puzzles/Day01.kt
@@ -12,12 +12,20 @@ object Day01 : Puzzle, Input by ReadInput.forDay(1) {
         .map { it.split(Regex("\\s+")).toPair().map(String::toInt) }
         .unzip()
 
+    // original O(n^2) solution
+    fun puzzleTwoOriginal() = inputs().run {
+        first.sumOf { l -> l * second.count { r -> l == r } }
+    }
+
     override fun puzzleOne() = inputs()
-            .map { it.sorted() }
-            .run { first.zip(second) { l, r -> abs( l - r) } }
-            .sum()
+        .map { it.sorted() }
+        .run { first.zip(second) { l, r -> abs(l - r) } }
+        .sum()
 
     override fun puzzleTwo() = inputs().run {
-        first.sumOf { l -> l * second.count { r -> l == r } }
+        val occurrences = buildMap {
+            for (id in second) compute(id) { _, v -> (v ?: 0) + 1 }
+        }
+        first.sumOf { it * (occurrences[it] ?: 0) }
     }
 }
