f2d9c063dcc0a75c75295a9485e3b93cab14dfcc
Fabian Klümpers <kluempers.fabian@gmail.com>
2024-12-04 18:15:28 +0100

    Implement Day04 part1

--- src/main/kotlin/kluempers/fabian/aoc/puzzles/Day04.kt
+++ src/main/kotlin/kluempers/fabian/aoc/puzzles/Day04.kt
@@ -3,10 +3,31 @@ package kluempers.fabian.aoc.puzzles
 import kluempers.fabian.aoc.Input
 import kluempers.fabian.aoc.Puzzle
 import kluempers.fabian.aoc.ReadInput
+import kluempers.fabian.aoc.utils.identity
 
-object Day04: Puzzle, Input by ReadInput.forDay(4) {
-    override fun puzzleOne(): Any? {
-        TODO("Not yet implemented")
+object Day04 : Puzzle, Input by ReadInput.forDay(4) {
+    override fun puzzleOne(): Int {
+        var count = 0
+        for (y in input.indices)
+            for (x in input[y].indices)
+                count += directionsToCheck.map { it(x, y, 4) }.count { it == "XMAS" || it == "SAMX" }
+        return count
+    }
+
+    private val directionsToCheck = listOf(::takeDiagonalRight, ::takeDiagonalLeft, ::takeRight, ::takeDown)
+
+    private fun takeDiagonalRight(x: Int, y: Int, length: Int) = take(x, y, length, Int::inc, Int::inc)
+    private fun takeDiagonalLeft(x: Int, y: Int, length: Int) = take(x, y, length, Int::dec, Int::inc)
+    private fun takeRight(x: Int, y: Int, length: Int) = take(x, y, length, Int::inc, ::identity)
+    private fun takeDown(x: Int, y: Int, length: Int) = take(x, y, length, ::identity, Int::inc)
+
+    private fun take(x: Int, y: Int, length: Int, modifyX: (Int) -> Int, modifyY: (Int) -> Int): String? {
+        if (y !in input.indices || x !in input[y].indices) {
+            return null
+        }
+        if (length <= 0) return null
+        if (length == 1) return input[y][x].toString()
+        return input[y][x].toString() + take(modifyX(x), modifyY(y), length - 1, modifyX, modifyY)
     }
 
     override fun puzzleTwo(): Any? {
--- src/main/kotlin/kluempers/fabian/aoc/utils/Utils.kt
+++ src/main/kotlin/kluempers/fabian/aoc/utils/Utils.kt
@@ -5,3 +5,5 @@ fun <T, R> Pair<T,T>.map(f: (T) -> R) = f(first) to f(second)
 fun <T> List<T>.toPair() = component1() to component2()
 
 fun <T> Pair<T,T>.toList() = listOf(first, second)
+
+fun <T> identity(t : T) = t
