ea512922708a2e6ad0982399c8dbbdbe76aa74f6
Fabian Klümpers <kluempers.fabian@gmail.com>
2024-12-07 13:23:15 +0100

    Add Day07 part 2

--- src/main/kotlin/kluempers/fabian/aoc/puzzles/Day07.kt
+++ src/main/kotlin/kluempers/fabian/aoc/puzzles/Day07.kt
@@ -4,15 +4,16 @@ import kluempers.fabian.aoc.Input
 import kluempers.fabian.aoc.Puzzle
 import kluempers.fabian.aoc.ReadInput
 
-object Day07: Puzzle, Input by ReadInput.forDay(7) {
-    override fun puzzleOne() =
-        input.map(Equation::fromString).filter(Equation::solvable).map(Equation::result).sum()
+object Day07 : Puzzle, Input by ReadInput.forDay(7) {
+    private val partOneOperators = listOf<(Long, Long) -> Long>(Long::times, Long::plus)
+
+    private fun solution(operators: List<(Long, Long) -> Long>) =
+        input.map(Equation::fromString).filter { it.solvableWith(operators) }.map(Equation::result).sum()
+
+    override fun puzzleOne() = solution(partOneOperators)
 
     private data class Equation(val result: Long, val numbers: List<Long>) {
-
-        val operators = listOf<(Long, Long) -> Long>(Long::times, Long::plus)
-
-        fun solvable() : Boolean =
+        fun solvableWith(operators: List<(Long, Long) -> Long>): Boolean =
             numbers.drop(1).fold(listOf(numbers.first())) { possibleResults, right ->
                 possibleResults.flatMap { left ->
                     operators.map { op ->
@@ -22,13 +23,11 @@ object Day07: Puzzle, Input by ReadInput.forDay(7) {
             }.contains(result)
 
         companion object {
-            fun fromString(s : String) = s.split(": ").let { (result, numbers) ->
+            fun fromString(s: String) = s.split(": ").let { (result, numbers) ->
                 Equation(result.toLong(), numbers.split(" ").map(String::toLong))
             }
         }
     }
 
-    override fun puzzleTwo(): Any? {
-        TODO("Not yet implemented")
-    }
+    override fun puzzleTwo() = solution(partOneOperators + { l, r -> (l.toString() + r.toString()).toLong() })
 }
